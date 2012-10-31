900e02d8f89e30dbfe5af82648d091f3c41e0e72
Arjan Scherpenisse <arjan@scherpenisse.net>
2012-10-31 11:11:09 +0100

    Also "make clean" on docs rebuild

--- modules/mod_zotonicdocs/mod_zotonicdocs.erl
+++ modules/mod_zotonicdocs/mod_zotonicdocs.erl
@@ -123,7 +123,7 @@ ensure_checkout(RootDir, Context) ->
     end.
 
 make_html(RootDir, Context) ->
-    os:cmd("cd " ++ z_utils:os_escape(filename:join(RootDir, "zotonic/doc")) ++ " && make stubs html"),
+    os:cmd("cd " ++ z_utils:os_escape(filename:join(RootDir, "zotonic/doc")) ++ " && make clean stubs html"),
     ?zInfo("sphinx docs built.", Context).
 
 make_edocs(RootDir, Context) ->
