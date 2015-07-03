f33f915ba195dacb86c9ac1a9419b94bdbd6203c
Marc Worrell <marc@worrell.nl>
2015-07-03 17:29:59 +0200

    Only generate docs for 0.13 and latest. Consider others as stable.

--- modules/mod_zotonicdocs/priv/build.sh
+++ modules/mod_zotonicdocs/priv/build.sh
@@ -7,7 +7,8 @@ ZOTONIC="$1"
 TARGET="$2"
 
 # which branches to build documentation for
-DOCBRANCHES="release-0.10.x release-0.11.x release-0.12.x release-0.13.x master"
+# DOCBRANCHES="release-0.10.x release-0.11.x release-0.12.x release-0.13.x master"
+DOCBRANCHES="release-0.13.x master"
 
 cd $ZOTONIC
 
